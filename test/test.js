d2e9455988b38c42acd1900433566c1623b79e3d
Antonio <antonio.cucciniello@nbcuni.com>
2017-09-18 09:06:03 -0400

    Add `Describe()` for all tests with no state
    	TODO:
    	- Repeat them for other states

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var bst = require('bespoken-tools')
 var assert = chai.assert
 var server = null
 var alexa = null
+var options = require('../helpers/course-summary-options.json')
 
 beforeEach(function (done) {
   var path = require('path')
@@ -37,106 +38,108 @@ afterEach(function (done) {
   })
 })
 
-it('Launches the skill', function (done) {
-  // Launch the skill via launch request
-  alexa.launched(function (error, payload) {
-    if (error) {
-      console.log(error)
+describe('Tests with No Inital State', function () {
+  it('Launches the skill', function (done) {
+    // Launch the skill via launch request
+    alexa.launched(function (error, payload) {
+      if (error) {
+        console.log(error)
+        done()
+      }
+      assert.equal(payload.response.outputSpeech.ssml, '<speak> Welcome to GolfNow.  You can book tee times at your favorite nearby courses.  You can say Alexa, ask GolfNow to book a tee time near me.  What would you like to do? </speak>')
+      assert.equal(payload.response.shouldEndSession, false)
       done()
-    }
-    assert.equal(payload.response.outputSpeech.ssml, '<speak> Welcome to GolfNow.  You can book tee times at your favorite nearby courses.  You can say Alexa, ask GolfNow to book a tee time near me.  What would you like to do? </speak>')
-    assert.equal(payload.response.shouldEndSession, false)
+    })
+  })
+
+  it('Launches the Help intent and doesnt end session', function (done) {
+    alexa.intended('AMAZON.HelpIntent', null, function (error, payload) {
+      if (error) {
+        console.log(error)
+        done()
+      }
+      assert.equal(payload.response.outputSpeech.ssml, '<speak> Welcome to Golf Now.  The purpose of this skill is to book a tee time at a Golf Course so you can start playing! To start using the skill, say Alexa, ask GolfNow to book a tee time near me. You can also search for tee times at a specific city or zip code. For city, you can say Alexa, ask GolfNow to book a tee time in Orlando. For a specific zipcode, you can say Alexa ask GolfNow to book a tee time near 32819. What would you like to do? </speak>')
+      assert.equal(payload.response.shouldEndSession, false)
+      done()
+    })
+  })
+
+  it('Stops and Exits Skill upon calling StopIntent', function (done) {
+    alexa.intended('AMAZON.StopIntent', null, function (error, payload) {
+      if (error) {
+        console.log(error)
+        done()
+      }
+      assert.equal(payload.response.outputSpeech.ssml, '<speak> Stopping your Request and Exiting Skill </speak>')
+      assert.equal(payload.response.shouldEndSession, true)
+      done()
+    })
+  })
+
+  it('Cancels and Exits Skill upon calling CancelIntent', function (done) {
+    alexa.intended('AMAZON.CancelIntent', null, function (error, payload) {
+      if (error) {
+        console.log(error)
+        done()
+      }
+      assert.equal(payload.response.outputSpeech.ssml, '<speak> Canceling your Request and Exiting Skill </speak>')
+      assert.equal(payload.response.shouldEndSession, true)
+      done()
+    })
+  })
+
+  it('Launches GetLocation with Utterance with ZipCode', function (done) {
+    alexa.spoken('book me a tee time near {32819}', function (error, response, request) {
+      if (error) {
+        console.log(error)
+      }
+      assert.equal(request.request.intent.name, 'GetLocation')
+    })
     done()
   })
-})
 
-it('Launches the Help intent and doesnt end session', function (done) {
-  alexa.intended('AMAZON.HelpIntent', null, function (error, payload) {
-    if (error) {
-      console.log(error)
-      done()
-    }
-    assert.equal(payload.response.outputSpeech.ssml, '<speak> Welcome to Golf Now.  The purpose of this skill is to book a tee time at a Golf Course so you can start playing! To start using the skill, say Alexa, ask GolfNow to book a tee time near me. You can also search for tee times at a specific city or zip code. For city, you can say Alexa, ask GolfNow to book a tee time in Orlando. For a specific zipcode, you can say Alexa ask GolfNow to book a tee time near 32819. What would you like to do? </speak>')
-    assert.equal(payload.response.shouldEndSession, false)
+  it('Launches GetLocation with Utterance with City', function (done) {
+    alexa.spoken('book me a tee time in {Orlando}', function (error, response, request) {
+      if (error) {
+        console.log(error)
+      }
+      assert.equal(request.request.intent.name, 'GetLocation')
+    })
     done()
   })
-})
 
-it('Stops and Exits Skill upon calling StopIntent', function (done) {
-  alexa.intended('AMAZON.StopIntent', null, function (error, payload) {
-    if (error) {
-      console.log(error)
-      done()
-    }
-    assert.equal(payload.response.outputSpeech.ssml, '<speak> Stopping your Request and Exiting Skill </speak>')
-    assert.equal(payload.response.shouldEndSession, true)
+  it('Launches GetLocation with Utterance with Near Me', function (done) {
+    alexa.spoken('book me a tee time {near me}', function (error, response, request) {
+      if (error) {
+        console.log(error)
+      }
+      assert.equal(request.request.intent.name, 'GetLocation')
+    })
     done()
   })
-})
 
-it('Cancels and Exits Skill upon calling CancelIntent', function (done) {
-  alexa.intended('AMAZON.CancelIntent', null, function (error, payload) {
-    if (error) {
-      console.log(error)
+  it('Response for GetLocation is Correct with zipcode', function (done) {
+    alexa.intended('GetLocation', { 'zipcode': '32819' }, function (error, payload) {
+      if (error) {
+        console.log(error)
+        done()
+      }
+      assert.equal(payload.response.outputSpeech.ssml, '<speak> What day would you like to play? </speak>')
+      assert.equal(payload.response.shouldEndSession, false)
       done()
-    }
-    assert.equal(payload.response.outputSpeech.ssml, '<speak> Canceling your Request and Exiting Skill </speak>')
-    assert.equal(payload.response.shouldEndSession, true)
-    done()
+    })
   })
-})
 
-it('Launches GetLocation with Utterance with ZipCode', function (done) {
-  alexa.spoken('book me a tee time near {32819}', function (error, response, request) {
-    if (error) {
-      console.log(error)
-    }
-    assert.equal(request.request.intent.name, 'GetLocation')
-  })
-  done()
-})
-
-it('Launches GetLocation with Utterance with City', function (done) {
-  alexa.spoken('book me a tee time in {Orlando}', function (error, response, request) {
-    if (error) {
-      console.log(error)
-    }
-    assert.equal(request.request.intent.name, 'GetLocation')
-  })
-  done()
-})
-
-it('Launches GetLocation with Utterance with Near Me', function (done) {
-  alexa.spoken('book me a tee time {near me}', function (error, response, request) {
-    if (error) {
-      console.log(error)
-    }
-    assert.equal(request.request.intent.name, 'GetLocation')
-  })
-  done()
-})
-
-it('Response for GetLocation is Correct with zipcode', function (done) {
-  alexa.intended('GetLocation', { 'zipcode': '32819' }, function (error, payload) {
-    if (error) {
-      console.log(error)
+  it('Response for GetLocation is Correct for city is correct', function (done) {
+    alexa.intended('GetLocation', { 'city': 'Orlando' }, function (error, payload) {
+      if (error) {
+        console.log(error)
+        done()
+      }
+      assert.equal(payload.response.outputSpeech.ssml, '<speak> What day would you like to play? </speak>')
+      assert.equal(payload.response.shouldEndSession, false)
       done()
-    }
-    assert.equal(payload.response.outputSpeech.ssml, '<speak> What day would you like to play? </speak>')
-    assert.equal(payload.response.shouldEndSession, false)
-    done()
-  })
-})
-
-it('Response for GetLocation is Correct for city is correct', function (done) {
-  alexa.intended('GetLocation', { 'city': 'Orlando' }, function (error, payload) {
-    if (error) {
-      console.log(error)
-      done()
-    }
-    assert.equal(payload.response.outputSpeech.ssml, '<speak> What day would you like to play? </speak>')
-    assert.equal(payload.response.shouldEndSession, false)
-    done()
+    })
   })
 })
 
@@ -237,6 +240,11 @@ it('Launches PriceReceived with Utterance of 40 dollars', function (done) {
 })
 
 it('Response for PriceReceived is correct', function (done) {
+  options.latitude = 40.896350
+  options.longitude = -73.941539
+  options.date = '2017-09-04'
+  options.time = '4:00'
+  options.numGolfers = '2'
   alexa._alexa._context._session.updateAttributes({'STATE': 'numGolfers'})
   alexa.intended('PriceReceived', { 'amountOfDollars': '50' }, function (error, payload) {
     if (error) {
