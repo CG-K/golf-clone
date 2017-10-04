0829954c5caaae31cdd875ae3c03e3cd8180424b
Antonio <antonio.cucciniello@nbcuni.com>
2017-10-04 18:18:01 -0400

    Successfully hit API
    	TODO:
    	- Parse the JSON to get correct data to output update format-course-summaries

--- helpers/get-course-summaries.js
+++ helpers/get-course-summaries.js
@@ -4,8 +4,8 @@ module.exports = getCourseSummaries
 
 const got = require('got')
 
-var createAuthToken = require('../create-auth-token.js')
-var handleCourseSummariesResponse = require('./handle-course-summaries-response.js')
+// var createAuthToken = require('../create-auth-token.js')
+// var handleCourseSummariesResponse = require('./handle-course-summaries-response.js')
 var createCourseSummariesURL = require('./create-course-summaries-url.js')
 
 // Purpose: To hit Golf Now API for course summaries with a given location
@@ -14,11 +14,17 @@ var createCourseSummariesURL = require('./create-course-summaries-url.js')
 // calledBy:  priceReceivedIntent
 function getCourseSummaries (options, callback) {
   var url = createCourseSummariesURL(options)
-  var token = createAuthToken()
+  /*var token = createAuthToken()
   token = 'Basic ' + token
   var urlOptions = {
     headers: {
       Authorization: token
+    }
+  }*/
+  var urlOptions = {
+    headers: {
+      UserName: process.env.USERNAME,
+      Password: process.env.PASSWORD
     }
   }
   console.log(urlOptions)
@@ -26,13 +32,15 @@ function getCourseSummaries (options, callback) {
   got(url, urlOptions)
     .then(response => {
       var parsedCourseResponse = JSON.parse(response.body)
+      console.log(response.body)
+      callback(null, 'we got the data')
       // take parsed response and generate a response for the user
-      handleCourseSummariesResponse(parsedCourseResponse, options.doNotRefine, function (err, output) {
+      /* handleCourseSummariesResponse(parsedCourseResponse, options.doNotRefine, function (err, output) {
         if (err) {
           callback(err)
         }
         callback(null, output)
-      })
+      }) */
     })
     .catch(error => {
       console.log(' we have an error' + error)
